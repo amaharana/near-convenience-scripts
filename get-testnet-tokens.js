ec59bbd5e46a76a30cb3497f1855cd4d0a5c7e2e
amaharana <amaharana@users.noreply.github.com>
2022-04-22 09:32:00 -0700

    Refactor and print start/end balance

--- get-testnet-tokens.js
+++ get-testnet-tokens.js
@@ -1,29 +1,42 @@
 
 // Script to create some NEAR dev accounts on testnet and transfer the balance to given account.
-
+const usageString = 'USAGE: node get-testnet-tokens.js <testnet_account_id_to_receive_tokens>';
 const { exit } = require('process');
 const { BN } = require('bn.js');
 const nearAPI = require("near-api-js");
 
 async function main() {
-    
     validateArguments();
-    const beneficiaryAccount = process.argv[2];
+    
     const config = getTestnetConfig(new nearAPI.keyStores.InMemoryKeyStore());
     const keyPair = nearAPI.KeyPair.fromRandom('ed25519');
     const near = await nearAPI.connect(config);
+    const beneficiaryAccountId = process.argv[2];
 
-    for (var i = 0; i < 1; i++) {
+    const startingBalance = await getNearBalance(near, beneficiaryAccountId);
+    console.log(`${beneficiaryAccountId} starting balance: ${startingBalance}`);
+
+    // stay under rate limit
+    for (var i = 0; i < 8; i++) {
         const throwawayAccountId = generateAccountId();
 
         console.log(`Creating account ${throwawayAccountId}`);
         await near.accountCreator.createAccount(throwawayAccountId, keyPair.publicKey);
         await config.keyStore.setKey(config.networkId, throwawayAccountId, keyPair);
 
-        console.log(`Deleting account ${throwawayAccountId} and transferring balance to ${beneficiaryAccount}`);        
+        console.log(`Deleting account ${throwawayAccountId} and transferring balance to ${beneficiaryAccountId}`);        
         const throwawayAccount = await near.account(throwawayAccountId);
-        await throwawayAccount.deleteAccount(beneficiaryAccount);
+        await throwawayAccount.deleteAccount(beneficiaryAccountId);
     }
+
+    const endingBalance = await getNearBalance(near, beneficiaryAccountId);
+    console.log(`${beneficiaryAccountId} ending balance: ${endingBalance}`);
+}
+
+async function getNearBalance(near, accountId) {
+    const account = await near.account(accountId);
+    const balance = (await account.getAccountBalance()).total;
+    return nearAPI.utils.format.formatNearAmount(balance);
 }
 
 function generateAccountId() {
@@ -46,7 +59,7 @@ function getTestnetConfig(keyStore) {
 function validateArguments() {
     // first arg is path to "node", second arg is script name
     if (process.argv.length != 3) {
-        console.log("USAGE: node get-testnet-tokens.js <testnet_account_id_to_receive_tokens>");
+        console.log(usageString);
         exit(1);
     }
 }
